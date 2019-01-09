55ed7fc94c3ec281415431342c53d1076de62b9f
Paul Jones <p-g-j@hotmail.com>
2019-01-09 21:10:19 +0000

    tests

--- demo/main.js
+++ demo/main.js
@@ -2,26 +2,57 @@
 
     const baseUrl = 'http://localhost:8081';
 
-    const call = (relPath, verb, json) => fetch({
-        url: `${baseUrl}/${relPath}`,
-        method: verb,
-        body: JSON.stringify(json)
-    });
+    const save_token = (token) => sessionStorage.setItem('token', token),
+          load_token = () => sessionStorage.getItem('token'),
+          wipe_token = () => sessionStorage.removeItem('token');
 
-    const loadToken = () => localStorage.getItem('token');
-    const saveToken = (token) => localStorage.setItem('token', token);
-    const wipeToken = () => localStorage.removeItem('token');
+    async function svc(secure, path, method, json) {
+
+        const url = `${baseUrl}/${path}`,
+              headers = { 'content-type': 'application/json' },
+              body = JSON.stringify(json);
+
+        if (secure === true) {
+            headers['authorization'] = `Bearer ${load_token()}`;
+        }
+
+        const response = await fetch(url, { method, headers, body }),
+              text = await response.text();
+
+        if (response.status === 401) {
+            return show_login(arguments);
+        }        
+        else if (!response.ok) {
+            throw text;
+        }
+
+        return JSON.parse(text);
+    }
+
+    function show_login(args) {
+        Array.from(document.querySelectorAll('.modal.open:not(.login)'))
+             .forEach(m => m.classList.remove('open'));
+        const modal = document.querySelector('.modal.login');
+        modal.classList.add('open');
+        modal.querySelector('[type=button]').onclick = () => {
+            const username = modal.querySelector('[placeholder=username]').value,
+                  password = modal.querySelector('[placeholder=password]').value;
+            svc(false, 'login', 'POST', { username, password })
+                .then(json => { save_token(json.token); return svc.apply(null, args); })
+                .catch(err => console.warn(err));
+        }
+    }
 
     window.addEventListener('load', () => {
-        document.querySelector
+
+        wipe_token();
+
+        const accounts_result = svc(true, 'account', 'POST')
+            .then(json => console.log('accounts!', json))
+            .catch(err => console.warn('aww maaan', err));
     });
 
-    function call_login(username, password) {
-        call('login', 'post', { username, password })
-            .then((response) => saveToken(response.text()))
-            .then(() => console.log('ok'))
-            .catch(err => console.error(err));
-    }
+    
 
 
 
